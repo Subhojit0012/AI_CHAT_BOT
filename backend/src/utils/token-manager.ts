2af55f92ddca77ca3d9773213e05150fb56c4611
Subhojit0012 <subhojitroy00012@gmail.com>
2024-07-10 22:14:33 +0530

    comment out

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -3,14 +3,19 @@ import { NextFunction, Request, Response } from 'express'
 import { COOKIE_NAME } from './constants.js'
 import { ifError } from 'assert'
 
+// Create Token:
 export const createToken = (id:string, email:string, expiresIn: string)=> {
-    const payload = {id, email}
+    const payload = {
+        id, 
+        email
+    }
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn,
     })
     return token
 }
 
+// verify Token:
 export const verifyToken = (req:Request, res:Response, next:NextFunction )=> {
     const token = req.signedCookies[`${COOKIE_NAME}`]
     if(!token || token.trim() === ""){
@@ -22,7 +27,7 @@ export const verifyToken = (req:Request, res:Response, next:NextFunction )=> {
                 reject(err.message);
                 return res.status(401).json({message: "Token expired"})
             } else {
-                // console.log("Token verification Successful")
+                console.log("Token verification Successful")
                 resolve()
                 res.locals.jwtData = success;
                 return next()
